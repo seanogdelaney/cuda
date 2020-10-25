62aa696db4bffd5c2df7d7de8b8eaa79da9af02d
seanogdelaney <seanogdelaney@users.noreply.github.com>
2020-10-25 12:23:17 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 git config --global user.email "seanogdelaney@gmail.com"
 git config --global user.name "Sean Delaney"
 sudo apt install vim
+cp .vimrc ~/.vimrc
 sudo apt-get purge nvidia*
 sudo apt-get autoremove
 sudo apt-get autoclean
@@ -11,5 +13,5 @@ sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
 sudo apt-get -y install cuda
-
-
+export PATH=/usr/local/cuda-11.1/bin:${PATH}
+export LD_LIBRARY_PATH=/usr/local/cuda-11.1/lib64:${LD_LIBRARY_PATH}
