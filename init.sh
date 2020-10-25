6609c725019c102264099119a840eb9f871d1104
Sean Delaney <seanogdelaney@gmail.com>
2020-10-25 14:10:34 +0000

    Removed cuda 11 installer

--- init.sh
+++ init.sh
@@ -2,16 +2,6 @@ PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[0
 git config --global user.email "seanogdelaney@gmail.com"
 git config --global user.name "Sean Delaney"
 sudo apt install vim
-cp .vimrc ~/.vimrc
-sudo apt-get purge nvidia*
-sudo apt-get autoremove
-sudo apt-get autoclean
-sudo rm -rf /usr/local/cuda-10.0
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-sudo apt-get update
-sudo apt-get -y install cuda
-export PATH=/usr/local/cuda-11.1/bin:${PATH}
-export LD_LIBRARY_PATH=/usr/local/cuda-11.1/lib64:${LD_LIBRARY_PATH}
+cp `dirname $0`/.vimrc ~/.vimrc
+export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
